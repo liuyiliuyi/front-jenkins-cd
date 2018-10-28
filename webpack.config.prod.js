2258e2812f82bfddee46d4d0c231f3fc6d1277ef
liuyiliuyi <452074104@qq.com>
2018-10-28 12:01:27 +0800

    test webhook

---
+++ webpack.config.prod.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './src',
+
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'index.js',
+    libraryTarget: 'umd',
+  },
+
+  externals: {
+    react: 'react',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader?minimize',
+            'postcss-loader',
+            'less-loader',
+          ],
+        }),
+      },
+      {
+        test: /\.(bmp|gif|png|jpe?g)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new ExtractTextPlugin('index.css'),
+  ],
+
+};
